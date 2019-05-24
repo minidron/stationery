2a28dabd4e5096df8c2297339861151202da5316
minidron <pavel.alekin@gmail.com>
2019-05-24 08:46:49 +0000

    Убираем старый код

--- stationery/apps/api/urls.py
+++ stationery/apps/api/urls.py
@@ -2,11 +2,10 @@ from rest_framework.routers import DefaultRouter
 
 from cart.api import CartViewSet
 
-from orders.api import AddressViewSet, OrderViewSet
+from orders.api import AddressViewSet
 
 
 router = DefaultRouter()
 
 router.register(r'addresses', AddressViewSet, base_name='addresses')
-router.register(r'orders', OrderViewSet)
 router.register(r'cart', CartViewSet, base_name='cart')
--- stationery/apps/orders/api.py
+++ stationery/apps/orders/api.py
@@ -2,110 +2,11 @@ from django.conf import settings
 
 import requests
 
-from rest_framework import authentication, permissions, status, viewsets
+from rest_framework import authentication, permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from orders.models import Order
-from orders.serializers import (
-    AddressSerializer, ItemSerializer, OrderSerializer)
-from orders.utils import fetch_delivery_price
-
-
-class OrderViewSet(viewsets.ModelViewSet):
-    """
-    ViewSet для модели `Order`.
-    """
-    serializer_class = OrderSerializer
-    queryset = Order.objects.prefetch_related('items')
-
-    authentication_classes = [
-        authentication.SessionAuthentication,
-        authentication.BasicAuthentication,
-    ]
-
-    permission_classes = [
-        permissions.IsAuthenticated,
-    ]
-
-    def get_queryset(self):
-        qs = super().get_queryset()
-        if not self.request.GET:
-            qs = qs.none()
-        return qs
-
-    def create(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    def partial_update(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    def list(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_400_BAD_REQUEST)
-
-    @action(detail=False, methods=['get'])
-    def cart(self, request):
-        """
-        Получаем информацию о корзине пользователя.
-        """
-        order = Order.get_cart(request.user)
-        serializer = self.get_serializer(order)
-        return Response(serializer.data)
-
-    @action(detail=False, methods=['post'])
-    def add_to_cart(self, request):
-        """
-        Добавляем товар в корзину.
-        """
-        offer_id = request.data.get('offer_id', None)
-        quantity = int(request.data.get('quantity', 0))
-
-        if not all([offer_id, quantity]):
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        order = Order.get_cart(request.user)
-        item = order.add_item(offer_id, quantity, user=request.user)
-        if not item:
-            return Response({'error': 'limit'}, status=status.HTTP_200_OK)
-
-        serializer = ItemSerializer(item)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    @action(detail=False, methods=['post'])
-    def remove_from_cart(self, request):
-        """
-        Удаляем товар из корзины.
-        """
-        offer_id = request.data.get('offer_id', None)
-
-        if not offer_id:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-
-        order = Order.get_cart(request.user)
-        order.remove_item(offer_id)
-        return Response(status=status.HTTP_200_OK)
-
-    @action(detail=False, methods=['post'])
-    def delivery_price(self, request):
-        """
-        Получаем цену доставки.
-        """
-        data = request.data
-        price = fetch_delivery_price(
-            data.get('delivery_type'),
-            '142200',
-            data.get('zip_code'),
-            2)  # Вес заказа
-        return Response({'price': price}, status=status.HTTP_200_OK)
+from orders.serializers import AddressSerializer
 
 
 class AddressViewSet(viewsets.ViewSet):
