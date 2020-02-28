49440f17ead7cfb7d4ac69f2ceb1b17ad59fb81b
minidron <pavel.alekin@gmail.com>
2020-02-28 07:44:33 +0000

    Подправил урлы

--- stationery/apps/pages/urls.py
+++ stationery/apps/pages/urls.py
@@ -4,20 +4,18 @@ from pages import views as views_pages
 
 
 urlpatterns = [
-    url(r'^поиск/$',
+    url(r'^search/$',
         views_pages.SearchOfferView.as_view(), name='search'),
-    url(r'^контакты/$',
+    url(r'^contacts/$',
         views_pages.OfficeListView.as_view(), name='contact'),
-    url(r'^мастер-классы/$',
+    url(r'^workshop/$',
         views_pages.BlogListView.as_view(), name='blog'),
-    url(r'^мастер-классы/(?P<pk>[0-9a-f-]+)/$',
+    url(r'^workshop/(?P<pk>[0-9a-f-]+)/$',
         views_pages.BlogDetailView.as_view(), name='blog_detail'),
-
-    url(r'^catalog/(?P<category_path>[-\w\/]+)/(?P<slug>[-\w]+)/$',
-        views_pages.ProductView.as_view(), name='product'),
-
     url(r'^catalog/(?P<path>[-\w\/]+)/$',
         views_pages.CategoryView.as_view(), name='category'),
+    url(r'^catalog/(?P<category_path>[-\w\/]+)/(?P<slug>[-\w]+)/$',
+        views_pages.ProductView.as_view(), name='product'),
     url(r'^(?P<slug>[-\w]+)/$',
         views_pages.PageView.as_view(), name='static'),
 ]
