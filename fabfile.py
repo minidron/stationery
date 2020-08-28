9393ee43d72e870a0805bd8bc00ce320e245d1cc
Pavel Alekin <pavel.alekin@gmail.com>
2020-08-28 20:52:50 +0000

    Немного наводим порядок в проекте

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,14 @@
-from stationery.apps.fabric_utils.fabric_class import (
-    add_class_methods_as_functions,
-    DjangoFabric
-)
+from fabric_class import DjangoFabric, add_class_methods_as_functions
 
 
 class Fabric(DjangoFabric):
-    host = '85.143.175.229'
     app_name = 'stationery'
-    repository = 'git@github.com:minidron/stationery.git'
-    remote_db_name = 'stationery_db'
+    host = '85.143.175.229'
     local_db_name = 'stationery'
+    remote_db_name = 'stationery_db'
+    repository = 'git@github.com:minidron/stationery.git'
     use_bower = True
+    user = 'mini'
 
 
 __all__ = add_class_methods_as_functions(Fabric(), __name__)
