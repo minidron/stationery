fb7739d364f46b03e38cb10855e13811826f8d98
enweave <enweave@gmail.com>
2021-01-29 17:34:15 +0500

    сигналы на индексацию

---
+++ stationery/apps/odinass/signals.py
@@ -0,0 +1,35 @@
+from odinass.models import Offer, Product, Category
+from odinass.documents import OfferDocType, CategoryDocType
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=Product)
+def update_product_offer_index(sender, instance, **kwargs):
+    OfferDocType().update(thing=instance.offers.all())
+
+
+@receiver(post_delete, sender=Product)
+def delete_product_offer_index(sender, instance, **kwargs):
+    OfferDocType().update(thing=instance.offers.all(), action='refresh')
+
+
+@receiver(post_save, sender=Offer)
+def update_offer_index(sender, instance, **kwargs):
+    OfferDocType().update(thing=instance)
+
+
+@receiver(post_delete, sender=Offer)
+def delete_offer_index(sender, instance, **kwargs):
+    OfferDocType().update(thing=instance, action='refresh')
+
+
+@receiver(post_save, sender=Category)
+def update_category_index(sender, instance, **kwargs):
+    CategoryDocType().update(thing=instance)
+
+
+@receiver(post_delete, sender=Category)
+def delete_category_index(sender, instance, **kwargs):
+    CategoryDocType().update(thing=instance, action='refresh')
