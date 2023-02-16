97c29690929693b172ab88a52c3b426b17461011
king130594 <web@quokka.pro>
2023-02-16 10:33:49 +0300

    ошибка fab и линк на картинку категории

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,7 @@ from fabric_class import DjangoFabric, add_class_methods_as_functions
 class Fabric(DjangoFabric):
     app_name = 'stationery'
     host = '85.143.175.229'
-    local_db_name = 'stationery'
+    local_db_name = 'stationery_db'
     remote_db_name = 'stationery_db'
     repository = 'git@github.com:minidron/stationery.git'
     use_bower = True
